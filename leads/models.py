1c9956e006c2211ef9f4d9a950f1a87b2d710e78
Gustavo Taques <gustavotaques@hotmail.com>
2021-08-13 17:50:41 -0300

    put special method __str__ in models

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.signals import user_logged_in
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -11,7 +10,13 @@ class Lead(models.Model):
     last_name = models.CharField(max_length= 20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
  
  
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
