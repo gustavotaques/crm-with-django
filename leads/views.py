644f7c4babc45ea8f79605600e08812655b69de9
Gustavo Taques <gustavotaques@hotmail.com>
2021-08-13 18:20:39 -0300

    change homepage render

--- leads/views.py
+++ leads/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'second_page.html')
+    return render(request, 'leads/home_page.html')
