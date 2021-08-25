ef303b099ed44295052d875be898da61dc22f52b
Gustavo Taques <gustavotaques@hotmail.com>
2021-08-25 14:29:44 -0300

    set some changes in leads app

--- crm/urls.py
+++ crm/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from leads.views import home_page
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page)
+    path('leads/', include('leads.urls', namespace='leads'))
+
 ]
