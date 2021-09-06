4e5a65582909e5843adc7549583b27c3adb319d9
Gustavo Taques <gustavotaques@hotmail.com>
2021-09-06 16:55:33 -0300

    create a signup CBV

--- leads/views.py
+++ leads/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views import generic
 from .models import Lead #Agent
-from .forms import LeadModelForm #LeadForm
+from .forms import LeadModelForm, CustomUserCreationForm #LeadForm
+
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")
 
 
 class LandingPageView(generic.TemplateView):
